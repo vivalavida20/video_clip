8df167fbe929d3064a2eb34124ad062a34bb7451
vivalavida20 <vivalavida2051@gmail.com>
2017-10-15 23:59:34 +0900

    sidebar에 들어갈 카테고리들을 모든 view에 각각 모두 포함시켜줌 (사이드바를 좀 더 효율적으로 관리할 수 있는 방법은 없는가..)

--- video_clip/views.py
+++ video_clip/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category, Clip
 from .form import ClipForm
 
-def index(request):
+def send_categories():
     categories = Category.objects.all()
+
+    return categories
+
+def index(request):
     clips = Clip.objects.all()
 
-    return render(request, "video_clip/index.html", {"categories":categories, "clips":clips})
+    return render(request, "video_clip/index.html", {"categories":send_categories(), "clips":clips})
 
 def new(request):
     if request.method == "POST":
@@ -16,11 +20,11 @@ def new(request):
             return redirect("index")
     else:
         form = ClipForm()
-        return render(request, "video_clip/form.html", {"form":form})
+        return render(request, "video_clip/form.html", {"categories":send_categories(), "form":form})
 
 def detail(request, pk):
     clip = Clip.objects.get(pk=pk)
-    return render(request, "video_clip/detail.html", {"clip":clip})
+    return render(request, "video_clip/detail.html", {"categories":send_categories(), "clip":clip})
 
 def edit(request, pk):
     clip = get_object_or_404(Clip, pk=pk)
@@ -32,7 +36,7 @@ def edit(request, pk):
             return redirect(clip)
     else:
         form = ClipForm(instance=clip)
-    return render(request, 'video_clip/form.html', {'form':form})
+    return render(request, 'video_clip/form.html', {"categories":send_categories(), 'form':form})
 
 
 def delete(request, pk):
@@ -45,5 +49,5 @@ def category(request, name):
     category = Category.objects.get(name=name)
     clips = Clip.objects.filter(category=category)
 
-    return render(request, 'video_clip/category_detail.html', {'clips':clips, 'category':category})
+    return render(request, 'video_clip/category_detail.html', {'clips':clips, 'category':category, "categories":send_categories()})
 
